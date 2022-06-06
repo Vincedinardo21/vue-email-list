02abde2ddb62fbad3063a30bfc249b01f2769651
vince <vincedinardo21@gmail.com>
2022-06-06 17:30:24 +0200

    Nuova lista errore

--- myscript.js
+++ myscript.js
@@ -1,17 +1,22 @@
 const app = new Vue({
     el : "#app",
     data : {
-        email : ""
+        emails : [
+            {
+                email : "",
+            }
+        ],
     },
     mounted(){
-        for(let i = 0; i < 10; i++){
+        for(let i = 0; i < 9; i++){
             axios.get("https://flynn.boolean.careers/exercises/api/random/mail")
             .then((result) => {
             let risultato = result.data.response;
             console.log(risultato);
-            this.email = risultato;
+            this.emails.email = risultato;
+            this.emails.push(risultato);        
         });
         }
-        
+        console.log(this.emails);            
     }
 });
