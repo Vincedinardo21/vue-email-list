fc99df55b7920ea7d7b76d2c4a5ed0571d8bbdba
vince <vincedinardo21@gmail.com>
2022-06-06 18:07:38 +0200

    Fix finale

--- myscript.js
+++ myscript.js
@@ -1,19 +1,16 @@
 const app = new Vue({
     el : "#app",
     data : {
-        emails : [
-            {
-                email : "",
-            }
-        ],
+        emails : [],
     },
     mounted(){
-        for(let i = 0; i < 9; i++){
+        for(let i = 0; i < 10; i++){
             axios.get("https://flynn.boolean.careers/exercises/api/random/mail")
             .then((result) => {
             let risultato = result.data.response;
             console.log(risultato);
-            this.emails.email = risultato;
+            //debugger;
+            //this.emails = risultato;
             this.emails.push(risultato);        
         });
         }
